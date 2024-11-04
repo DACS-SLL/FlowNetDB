f845ae69603d71dba34c9f474807a4c9a52ebf0b
AndreaVelazco <104018328+AndreaVelazco@users.noreply.github.com>
2024-11-03 22:07:09 -0500

    Agreacion de %ROWTYPE, %TYPE

--- Scripts/Funciones_Procedimientos/Procedimientos_func.sql
+++ Scripts/Funciones_Procedimientos/Procedimientos_func.sql
@@ -156,3 +156,61 @@ JOIN Empleado e ON v.id_empleado = e.id_empleado
 WHERE MONTH(v.fecha) = @mes AND YEAR(v.fecha) = @anio;
 END;
 GO
+
+-- AgreaciOn de %ROWTYPE, %TYPE
+  
+CREATE OR REPLACE PROCEDURE insertar_cliente_nuevo IS
+    nuevo_cliente Cliente%ROWTYPE;  
+BEGIN
+    -- Asignar valores a los campos del cliente nuevo
+    nuevo_cliente.nombre := 'Carlos Gómez';
+    nuevo_cliente.telefono := '987654321';
+    nuevo_cliente.direccion := 'Calle Ejemplo 123';
+    nuevo_cliente.email := 'carlos.gomez@mail.com';
+    nuevo_cliente.historia_compras := NULL;  
+
+    INSERT INTO Cliente VALUES nuevo_cliente;
+END;
+
+CREATE OR REPLACE FUNCTION verificar_cliente_existente(
+    p_nombre Cliente.nombre%TYPE,  
+    p_email Cliente.email%TYPE     
+) RETURN BOOLEAN IS
+    cliente_existente BOOLEAN;
+BEGIN
+    SELECT COUNT(*) > 0 INTO cliente_existente
+    FROM Cliente
+    WHERE nombre = p_nombre AND email = p_email;
+
+    RETURN cliente_existente;
+END;
+
+
+CREATE OR REPLACE PROCEDURE registrar_venta_detallada(
+    p_nombre_cliente Cliente.nombre%TYPE,  
+    p_modelo_vehiculo Vehiculo.modelo%TYPE,  
+    p_precio_detalle Venta.precio%TYPE  
+) IS
+    nueva_venta Venta%ROWTYPE;   
+    id_vehiculo Vehiculo.id_vehiculo%TYPE;  
+BEGIN
+    SELECT id_vehiculo INTO id_vehiculo
+    FROM Vehiculo
+    WHERE modelo = p_modelo_vehiculo;
+
+    nueva_venta.id_cliente := (SELECT id FROM Cliente WHERE nombre = p_nombre_cliente);
+    nueva_venta.id_empleado := (SELECT id FROM Empleado WHERE nombre = 'Carlos Gómez');
+    nueva_venta.tipo_comprobante := 'Factura';
+    nueva_venta.fecha := SYSDATE;
+    nueva_venta.pago_inicial := 3000.00;
+    nueva_venta.metodo_pago := 'Efectivo';
+
+    INSERT INTO Venta VALUES nueva_venta;
+
+    INSERT INTO Detalle_Venta (id_venta, id_vehiculo, precio)
+    VALUES (
+        nueva_venta.id_venta,
+        id_vehiculo,
+        p_precio_detalle
+    );
+END;
