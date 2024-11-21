7c8f774d77502d67602b3b32aefe4158d3df0f1e
DACS-SLL <130502886+DACS-SLL@users.noreply.github.com>
2024-11-21 13:00:41 -0500

    Update usuarios.sql

--- Scripts/CreacionBD/usuarios.sql
+++ Scripts/CreacionBD/usuarios.sql
@@ -1,7 +1,6 @@
 USE FlowNet;
 GO
 
--- Crear usuarios con contraseñas
 CREATE LOGIN administrador WITH PASSWORD = 'AdminDcs';
 CREATE USER administrador FOR LOGIN administrador;
 
@@ -17,20 +16,15 @@ CREATE USER empleado1 FOR LOGIN empleado1;
 CREATE LOGIN empleado2 WITH PASSWORD = 'EmplAlv';
 CREATE USER empleado2 FOR LOGIN empleado2;
 
--- Asignar roles y permisos al administrador
 ALTER ROLE db_owner ADD MEMBER administrador;
 
--- Asignar permisos al analista de datos
 ALTER ROLE db_datareader ADD MEMBER analista_datos;
 ALTER ROLE db_datawriter ADD MEMBER analista_datos;
 
--- Asignar permisos al revisor
 ALTER ROLE db_datareader ADD MEMBER revisor;
 
--- Asignar permisos a empleado1
 ALTER ROLE db_datareader ADD MEMBER empleado1;
 ALTER ROLE db_datawriter ADD MEMBER empleado1;
 
--- Asignar permisos a empleado2
 ALTER ROLE db_datareader ADD MEMBER empleado2;
 ALTER ROLE db_datawriter ADD MEMBER empleado2;
