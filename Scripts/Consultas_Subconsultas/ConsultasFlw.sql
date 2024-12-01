203a1dde2a5404db49c6ab5f8d202086eb49aa27
DACS-SLL <130502886+DACS-SLL@users.noreply.github.com>
2024-11-30 20:27:58 -0500

    Consultas2

--- Scripts/Consultas_Subconsultas/ConsultasFlw.sql
+++ Scripts/Consultas_Subconsultas/ConsultasFlw.sql
@@ -23,7 +23,6 @@ WHERE ev.id_empleado = 5555 -- Reemplazar 5555 con el ID del empleado
 AND v.fecha BETWEEN '2024-01-01' AND '2024-12-31'; -- Reemplazar con el rango que se desea
 
 -- Calcular el descuento promedio en ventas
-
 SELECT AVG((v.precio - dv.pago_inicial) / v.precio * 100) AS descuento_promedio
 FROM Vehiculo v
 JOIN Venta dv ON v.id_vehiculo = dv.id_vehiculo;
@@ -48,6 +47,56 @@ SELECT COUNT(*) > 0 AS cliente_existente
 FROM Cliente
 WHERE nombre = 'Juan Pérez' -- Reemplazar con el nombre del cliente
 AND email = 'juan.perez@example.com'; -- Reemplazar con el email del cliente
+
+-- Consulta para obtener la información de una venta específica
+SELECT v.id_venta,
+       v.fecha,
+       e.nombre AS empleado_nombre,
+       tc.tipo AS tipo_comprobante,
+       dv.id_vehiculo,
+       ve.precio AS precio_vehiculo,
+       dv.pago_inicial,
+       dv.saldo_pendiente,
+       dv.id_cliente,
+       c.nombre AS cliente_nombre
+FROM Venta v
+INNER JOIN Empleado e ON v.id_empleado = e.id_empleado
+INNER JOIN Tipo_Comprobante tc ON v.id_tipoC = tc.id_tipoC
+INNER JOIN DetalleVenta dv ON v.id_venta = dv.id_venta
+INNER JOIN Vehiculo ve ON dv.id_vehiculo = ve.id_vehiculo
+INNER JOIN Cliente c ON dv.id_cliente = c.id_cliente
+WHERE v.id_venta = @id_venta;
+
+-- Reporte de ventas mensuales
+SELECT
+    v.id_venta,
+    c.nombre AS nombre_cliente,
+    e.nombre AS nombre_empleado,
+    v.fecha,
+    v.pago_inicial + v.saldo_pendiente AS total_pagado,
+    v.metodo_pago
+FROM Venta v
+JOIN Cliente c ON v.id_cliente = c.id_cliente
+JOIN Empleado e ON v.id_empleado = e.id_empleado
+WHERE MONTH(v.fecha) = @mes 
+  AND YEAR(v.fecha) = @anio;
+
+-- Obtener ID de vehículo por modelo
+SELECT id_vehiculo 
+INTO id_vehiculo
+FROM Vehiculo
+WHERE modelo = p_modelo_vehiculo;
+
+-- Obtener ID de cliente por nombre
+SELECT id 
+FROM Cliente 
+WHERE nombre = p_nombre_cliente;
+
+-- Obtener ID de empleado por nombre (en este caso, 'Carlos Gómez')
+SELECT id 
+FROM Empleado 
+WHERE nombre = 'Carlos Gómez';
+
 
 
 
